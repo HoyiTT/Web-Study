dd55ea6f24aa29a5853f169a74e30876e8038140
박 준서 <pjs9177@naver.com>
2021-07-20 05:14:05 +0000

    210720v2

---
+++ JS/const.js
@@ -0,0 +1,13 @@
+let name = "Mike";
+const AGE = 30;
+const name2 = 'Mike';
+const name3 = `Mike`;
+
+const message1 = "I'm a boy.";
+const message2 = 'I\'m a boy.';
+
+const message3 = `My name is ${name}`;
+//console.log(name);
+console.log(message3)
+const message4 = `내 나이는 ${30+1}살입니다.`;
+console.log(message4)
