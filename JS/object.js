f0bfdbe25b71639b7134ccf59427abd465922015
박 준서 <pjs9177@naver.com>
2021-07-27 01:23:58 +0000

    210727v4

---
+++ JS/object.js
@@ -0,0 +1,12 @@
+const superman = {
+    name : 'clark',
+    age : 30,
+    
+}
+
+superman.hairColor = 'balck';
+superman['hobby'] = 'football';
+
+console.log(superman.name);
+delete superman.age;
+console.log(superman);
