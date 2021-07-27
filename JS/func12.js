963150caf5bc133a4bcce22acd1723235687a781
박 준서 <pjs9177@naver.com>
2021-07-27 01:17:25 +0000

    210727

--- JS/func12.js
+++ JS/func12.js
@@ -2,8 +2,14 @@ function sayHello(){
     console.log('Hello');
 }
 //함수 선언문
+/*
+함수 선언문 : 어디서든 호출가능 -> 호이스팅
+*/
 
 let sayHello = function(){
     console.log('Hello');
 }
 //함수 표현식
+/*
+함수 표현식 : 코드에 도달하면 생성
+*/
