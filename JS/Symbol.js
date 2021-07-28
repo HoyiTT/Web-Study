6e578d764da2c09831a67b7c660a074ec97e93ec
박 준서 <pjs9177@naver.com>
2021-07-28 02:55:43 +0000

    JS 심볼설명추가

--- JS/Symbol.js
+++ JS/Symbol.js
@@ -12,7 +12,9 @@ const showName = Symbol('show name');
 user [showName] = function () {
     console.log(this.name);
 };
-
+/*심볼을 사용하면 Object.key , Object.values 와 같은 함수를 사용해도
+값이 보이지 않음 -> 여러값을 추가하는 데 유용함
+*/
 
 
 //사용자가 접속하면 보는 메시지
