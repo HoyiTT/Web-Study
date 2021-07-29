608a3fe5f77db9e5a3a2a60916ec6e379ac97f92
박 준서 <pjs9177@naver.com>
2021-07-29 01:31:31 +0000

    210729

---
+++ JS/numbermath.js
@@ -0,0 +1,12 @@
+toString()
+//10wlstn -> 2/16진수
+let num = 10;
+num.toString(); // "10"
+num.toString(2); // "1010"
+
+Math.ceil() // 올림
+
+let num2 = 5.7;
+Math.ceil(num2); //6
+
+Math.round() // 라운드
