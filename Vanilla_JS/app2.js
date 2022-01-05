84ad6cc667bbc08d6065ae49b9f44bc7b847accd
박 준서 <pjs9177@naver.com>
2022-01-05 14:31:48 +0000

    CSS in JS 3(toggle)

--- Vanilla_JS/app2.js
+++ Vanilla_JS/app2.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector((".hello h1 "))
 
 function handleTitleClick(){
-    const clickedClass = "clicked";
-    if(h1.className === clickedClass){
-        h1.className = "";
-    } else {
-        h1.className= clickedClass;
-    }
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
