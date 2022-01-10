9116f6d71fce85734bb6708db9e4fe3313b8ec1c
박 준서 <pjs9177@naver.com>
2022-01-10 13:39:06 +0000

    JS preventDefault

--- Vanilla_JS/app4.js
+++ Vanilla_JS/app4.js
@@ -1,10 +1,12 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
 
-function onLoginBtnClick() {
+
+function onLoginSubmit(event) {
+    event.preventDefault();
     const username = loginInput.value;
     console.log(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
