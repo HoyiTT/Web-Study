08e3d4715c420e64835474278b2c5e89754d1838
박 준서 <pjs9177@naver.com>
2022-01-11 13:47:19 +0000

    JS Getting username

--- Vanilla_JS/app4.js
+++ Vanilla_JS/app4.js
@@ -1,18 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log(username);
-}
-
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
