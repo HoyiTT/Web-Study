7df079ac92f1377d7e748f3004a18bfd055a3228
박 준서 <pjs9177@naver.com>
2022-01-21 04:50:30 +0000

    JS PadStart

--- Vanilla_JS/chapter_5/js/clock.js
+++ Vanilla_JS/chapter_5/js/clock.js
@@ -2,7 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    const hour = String(date.getHours()).padStart(2,"0");
+    const min = String(date.getMinutes()).padStart(2,"0");
+    const sec = String(date.getSeconds()).padStart(2,"0");
+    
+    clock.innerText = (`${hour}:${min}:${sec}`);
 }
 getClock();
 setInterval(getClock, 1000);
