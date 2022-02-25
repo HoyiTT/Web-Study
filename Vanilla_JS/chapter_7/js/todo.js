94dbe62d7335d262bf8f9fef298964f940d39e21
박 준서 <pjs9177@naver.com>
2022-02-25 01:09:53 +0000

    JS Adding To Dos

--- Vanilla_JS/chapter_7/js/todo.js
+++ Vanilla_JS/chapter_7/js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDO(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
-    console.log(newTodo, toDoInput.value);
+    paintToDO(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
