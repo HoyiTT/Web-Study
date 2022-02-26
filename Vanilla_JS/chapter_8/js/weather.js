a8fa263a556eb6e01fd49fb105e6a8d9108cf8c4
박 준서 <pjs9177@naver.com>
2022-02-26 11:08:49 +0000

    JS Weather API

--- Vanilla_JS/chapter_8/js/weather.js
+++ Vanilla_JS/chapter_8/js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "1ff3a946ee15b2897f110367da88c8c8";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log('You live in', lat, lng);
+    const lon = position.coords.longitude;
+    console.log('You live in', lat, lon);
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).
+    then(response => response.json()).
+    then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
