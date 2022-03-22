db0253963a136efb9a8fe4eb3d7d81d5abae2d68
박 준서 <pjs9177@naver.com>
2022-03-22 10:59:53 +0000

    React useEffect

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("i run all th time");
+
+  useEffect(() => {
+    console.log("call the api");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
