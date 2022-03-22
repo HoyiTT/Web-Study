bb6a95bd262507af941e37963710d285c1cdb195
박 준서 <pjs9177@naver.com>
2022-03-22 11:43:01 +0000

    React useEffect 4

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
 
 function Hello() {
-  function byFn() {
-    console.log("bye :(");
-  }
-  function hiFn() {
-    console.log("created :)")
-    return byFn;
-  }
-  // 파괴될때에도 알고싶은경우 리턴값에 결과를 출력하는 함수를 넣어준다.
-  useEffect(hiFn, []);
+useEffect(() => {
+  console.log("hi :)");
+  return () => console.log("bye (:");
+}, []);
+
+// useEffect(function() {
+//   console.log("hi :)");
+//   return function() {
+//     console.log("bye :(");
+//   }
+//   }, []);
+
   return <h1>Hello</h1>
 }
 
