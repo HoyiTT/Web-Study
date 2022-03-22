69d8788f5057a40ffe6c9ea3582846c1287fcc7b
박 준서 <pjs9177@naver.com>
2022-03-22 11:17:42 +0000

    React useEffect 2

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -2,14 +2,36 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all th time");
+  const onChange= (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log("call the api");
+    console.log("i run only once");
   }, []);
+// useEffect는 한 번만 실행되기를 원할떄 사용함
+useEffect(() => {
+  console.log("i run when 'keyword' changes");
+}, [keyword]);
+
+useEffect(() => {
+  console.log("i run when 'counter' changes");
+}, [counter]);
+
+useEffect(() => {
+  console.log("i run when keyword & counter changes");
+}, [keyword, counter]);
+  //2번째 인자로 배열에 원하는 키워드(변수)를 넣어주면 해당 변수가 변화 할 때에만 실행하게끔 해줌
+
+
   return (
     <div>
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
