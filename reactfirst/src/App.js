2541ca974dfe86f24e56a05813ab04d46034b35d
박 준서 <pjs9177@naver.com>
2022-03-23 01:37:00 +0000

    React To Do 2

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -2,16 +2,20 @@ import {useState} from "react";
 
 function App() {
   const [toDo,setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === ""){
       return;
     }
-    setToDo("")
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
   }
+  console.log(toDos);
   return (
     <div>
+      <h1>My To DOS ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input 
         onChange={onChange} 
