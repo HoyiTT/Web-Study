9c36cf01ca145184114e145821647027b74c3429
박 준서 <pjs9177@naver.com>
2022-03-23 01:25:30 +0000

    React To Do 1

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -1,29 +1,26 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-useEffect(() => {
-  console.log("hi :)");
-  return () => console.log("bye (:");
-}, []);
-
-// useEffect(function() {
-//   console.log("hi :)");
-//   return function() {
-//     console.log("bye :(");
-//   }
-//   }, []);
-
-  return <h1>Hello</h1>
-}
-
+import {useState} from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo,setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === ""){
+      return;
+    }
+    setToDo("")
+  }
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
