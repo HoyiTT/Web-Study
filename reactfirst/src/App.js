5134b0c10a02546968640bd60c58411ed9dac417
박 준서 <pjs9177@naver.com>
2022-03-23 03:53:38 +0000

    Coin tracker Version2

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -13,11 +13,12 @@ function App() {
   }, [])
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading..</strong> : null}
-      <ul>
-        {coins.map((coin) => <li>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</li>)}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})` }</h1>
+      {loading ? <strong>Loading..</strong> : 
+      <select>
+        {coins.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
+      </select>}
+      
     </div>
   );
 }
