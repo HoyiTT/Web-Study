340f344a4a276f11925450e8fa31764fd6ba2e0f
박 준서 <pjs9177@naver.com>
2022-03-25 04:54:27 +0000

    React Movie App part One

--- reactfirst/src/App.js
+++ reactfirst/src/App.js
@@ -1,26 +1,43 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins,setCoins] = useState([]);
-  useEffect( ()=> {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    });
-  }, [])
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})` }</h1>
-      {loading ? <strong>Loading..</strong> : 
-      <select>
-        {coins.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
-      </select>}
-      
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
--- reactfirst/src/Coin tracker_2.js
+++ reactfirst/src/Coin tracker_2.js
@@ -1,64 +1,47 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins,setCoins] = useState([]);
-  const [usd,setUsd] =useState(0);
-  const onChange= (e) => {
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState(0);
+  const onChange = (e) => {
     setUsd(e.target.value);
     console.log(usd);
-  }
-  useEffect( ()=> {
+  };
+  useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    });
-  }, [])
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})` }</h1>
-      {loading ? <strong>Loading...</strong> :
-      (<div>
-        <input 
-        type="number" 
-        placeholder="How mush do you have" 
-        value={usd}
-        onChange={onChange}></input>
-        <br></br>
-        {(usd !== 0) ? (`You have ${usd}$`) : ""}
-        <select>
-          {coins.map((coin) => <option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
-        </select>
-      </div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <input
+            type="number"
+            placeholder="How mush do you have"
+            value={usd}
+            onChange={onChange}
+          ></input>
+          <br></br>
+          {usd !== 0 ? `You have ${usd}$` : ""}
+          <select>
+            {coins.map((coin) => (
+              <option>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        </div>
       )}
-      
     </div>
   );
 }
 
 export default App;
-
-// { <div>
-//       <h1>The Coins!{coins.length}</h1>
-//       {loading ? (
-//         <strong>Loading...</strong>
-//       ) : (<div><input
-//         value={dollar}
-//         onChange={onChange}
-//         type='number'
-//         placeholder='How much do you have'
-//       ></input>
-//      <div>{dollar}</div> 
-//         <select>
-//           {coins.map((coin) => (
-//             <option>
-//               {coin.name}({coin.symbol}):${dollar / coin.quotes.USD.price}USD
-//             </option>
-//           ))}
-//         </select>
-//         </div>
-//       )}
-      
-//     </div> }
